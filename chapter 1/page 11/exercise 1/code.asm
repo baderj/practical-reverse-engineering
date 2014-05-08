d91f000800b7dd4b7840d979c6de07d82a9fe5a4
Johannes Bader <baderj@gmx.net>
2014-05-08 18:00:24 +0200

    moved commented version to separate file

--- chapter 1/page 11/exercise 1/code.asm
+++ chapter 1/page 11/exercise 1/code.asm
@@ -5,35 +5,31 @@ SECTION  .text
 GLOBAL _start
 _start: 
     nop
-    push byte 'x'      ; second function parameter
-    push dword my_str  ; first function parameter
-    call black_out     ; call function
-    add esp, 4         ; cleaning out the stack
-    mov  ebx,0         ; parameter for exit call (return value) 
-    mov  eax,1         ; exit system call
-    int 080h           ; run system call, see page 79 pal
+    push byte 'x'      
+    push dword my_str  
+    call black_out     
+    add esp, 4         
+    mov  ebx,0         
+    mov  eax,1         
+    int 080h           
 
 black_out:
-    push ebp           ; function prologue, save stack base pointer
-    mov ebp, esp       ; point base pointer to ESP
+    push ebp           
+    mov ebp, esp       
     ; ------------ start code from book ---------
-    mov edi, [ebp+8]   ; put first parameter (char*) in edi 
-    mov edx, edi       ; make a copy of edi (scasb will change edi)
-    xor eax, eax       ; set eax to 0 (i.e., null byte)
-    or ecx, 0FFFFFFFFh ; set ecs to 0xFFFFFFFF = (-1)
-    repne scasb        ; search eax (null byte) in memory at edi (first
-                       ; parameter), decrement ecx for each tested char.
-    add ecx, 2         ; increment ecx by 2 (to compensate for starting
-                       ; at -1, and the final null byte
-    neg ecx            ; change sign of ecx. ecx is now length of string
-    mov al, [ebp+0Ch]  ; copy second parameter (char) to al (byte)
-    mov edi, edx       ; restore edi from backup (points at string again)
-    rep stosb          ; write byte at eax (second function parameter) to
-                       ; memory at edi (first function parameter), for edi
-                       ; times (length of string)
-    mov eax, edx       ; set eax (return value) to address of (changed) string
+    mov edi, [ebp+8]   
+    mov edx, edi       
+    xor eax, eax       
+    or ecx, 0FFFFFFFFh 
+    repne scasb        
+    add ecx, 2         
+    neg ecx            
+    mov al, [ebp+0Ch]  
+    mov edi, edx       
+    rep stosb          
+    mov eax, edx       
     ; ------------ end code from book -----------
-    mov esp, ebp       ; restore stack pointer
-    pop ebp            ; restore stack base pointer
+    mov esp, ebp       
+    pop ebp            
     ret
 
